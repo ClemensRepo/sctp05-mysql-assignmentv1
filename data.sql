f20edbcec3edd5160e04dcc15d4e7125ac5f0cf7
Clemens Lee <operations.sg65@outlook.com>
2025-01-01 14:18:01 +0000

    Update data.sql

--- data.sql
+++ data.sql
@@ -15,21 +15,21 @@ INSERT INTO Departments (name) VALUES
 
 -- Inserting data into Employees
 INSERT INTO Employees (first_name, last_name, department_id) VALUES
-('Robert', 'Brown', 1),
-('Emily', 'Davis', 1),
-('Michael', 'Wilson', 2);
+('Ming', 'Kuan', 1),
+('Hwee', 'Lan', 1),
+('Johnny', 'Goh', 2);
 
 -- Inserting data into Products
 INSERT INTO Products (name, description) VALUES
-('Investment Plan A', 'Comprehensive investment plan for long-term growth'),
-('Retirement Fund B', 'Retirement fund with a focus on stability and steady income'),
-('Equity Package C', 'Diverse equity package for aggressive growth');
+('Blender', 'Multi-purpose blender to blend and mix fruits'),
+('Blackforest Cake', 'Cake suitable for occasions like birthday celebrations'),
+('Chicken Wings', 'Imported Brazillian chicken wings');
 
 -- Inserting data into Sales
 INSERT INTO Sales (product_id, employee_id, customer_id, quantity, sale_date) VALUES
-(1, 1, 1, 10, '2024-01-15'),
-(2, 1, 2, 5, '2024-01-20'),
-(3, 2, 3, 15, '2024-01-25');
+(1, 1, 1, 10, '2024-12-18'),
+(2, 1, 2, 5, '2024-11-02'),
+(3, 2, 3, 15, '2024-12-29');
 
 -- Inserting data into EmployeeCustomer
 INSERT INTO EmployeeCustomer (employee_id, customer_id) VALUES
